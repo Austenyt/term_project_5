f80182dba50745c513794d24b9e4de6a53d8f8b8
Марат Мусин <austenyt@gmail.com>
2023-09-15 00:10:45 +0300

    main file and sql

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.dbmanager import DBManager
+from src.hh import HeadHunter
+
+hh = HeadHunter()
+hh.get_vacancies()
+hh_data = hh.get_format_and_search_vacancies()
+
+db = DBManager()
+db.fill_tables_from_files(hh_data)
+db.get_companies_and_vacancies_count()
+db.get_all_vacancies()
+db.get_avg_salary()
+db.get_vacancies_with_higher_salary()
+db.get_vacancies_with_keyword()
